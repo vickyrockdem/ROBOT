9d43bae2d52d0ca4b0e3c457e8fd4df29e9f4113
Sivadeep Behera <sivadeep.behera@ad.infosys.com>
2020-05-26 15:10:52 +0530

    commit

--- buildAndReleaseTask/index.ts
+++ buildAndReleaseTask/index.ts
@@ -3,38 +3,11 @@
 import tl = require('azure-pipelines-task-lib/task');
 var request = require("request");
 
-//This is the function whaich needs to be executed
-
-async function run() {
-    try {
-      //Getting inputs from the users
-      var TestcasePath: string | undefined = tl.getInput("TestCasePath", true);
-      var RunIndividualTestCase: string | undefined = tl.getInput("RunIndividualTestCase", false);
-      var TestCaseName: string | undefined = tl.getInput("TestCaseName", true);
-      
-       await details({ TestCasePath: TestcasePath, RunIndividualTestCase, TestCaseName }) ;
-       await details(
-         { TestCasePath: TestcasePath, RunIndividualTestCase, TestCaseName });
-      
-     } 
-        catch (err) {
-        console.log("error in input");
-        
-        var output: string;
-        var sliced: string;
-    }
-
-     }
-      run();
-
-        async function details(
-{ TestCasePath, RunIndividualTestCase, TestCaseName }: { TestCasePath: string | undefined; RunIndividualTestCase: string | undefined; TestCaseName: string | undefined; }      )
-        
-        {
+{
           console.log ("calling clild_process");
 
         const { exec } = require('child_process');
-        exec( 'robot -t ' + ' ' + TestCasePath, 
+        exec( 'robot Testcases.robot', 
         {'shell':'powershell.exe'}, (error: any, stdout: any, stderr: any)=>{
         
         
