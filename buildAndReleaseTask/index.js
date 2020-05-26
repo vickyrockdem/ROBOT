55634711101071016461ab5166e8f3f60b4f4094
Sivadeep Behera <sivadeep.behera@ad.infosys.com>
2020-05-26 17:11:40 +0530

    commit

--- buildAndReleaseTask/index.js
+++ buildAndReleaseTask/index.js
@@ -1,9 +1,8 @@
 "use strict";
 //This index.ts file is about creating a custom task in marketplace
-Object.defineProperty(exports, "__esModule", { value: true });
-var request = require("request");
+//import tl = require('azure-pipelines-task-lib/task');
+//var request = require("request");
 {
-    console.log("calling clild_process");
     var exec = require('child_process').exec;
     exec('robot Testcases.robot', { 'shell': 'powershell.exe' }, function (error, stdout, stderr) {
     });
